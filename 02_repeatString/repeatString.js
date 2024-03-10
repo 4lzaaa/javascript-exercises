d55fad59ae95b43f9a7e64440471fcd4f92bee2c
4lza <aleezaahmed315@gmail.com>
2024-03-10 14:55:24 +0000

    completed 2 tasks from assignment

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,10 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+
+    if ((str == null) || (num === Infinity) || (num === null) || (num < 0)) {
+        return "ERROR"
+    }; 
+
+    return new Array(num + 1).join(str);
 
 };
 
